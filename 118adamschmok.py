208e4dc59a8225d62f574f92473409eb69bb978b
pickle <55500841+pickles9@users.noreply.github.com>
2019-10-10 11:36:27 -0400

    Initial upload

---
+++ 118adamschmok.py
@@ -0,0 +1,13 @@
+import turtle as trtl # Imports turtle
+tboy = trtl.Turtle() # Makes turtle
+tboy.hideturtle()
+
+window_color = 255,255,255
+window_border = 207,225,244
+edge_color = 102,112,124
+
+tboy.pencolor("#4a525b")
+tboy.forward(274)
+
+wn = trtl.Screen()
+wn.mainloop()
