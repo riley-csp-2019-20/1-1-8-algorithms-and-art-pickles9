247106b3b6e9d84499d7891a4c624b9a837d4c08
pickle <55500841+pickles9@users.noreply.github.com>
2019-10-11 11:36:46 -0400

    Draws square and small square

--- 118adamschmok.py
+++ 118adamschmok.py
@@ -1,13 +1,38 @@
 import turtle as trtl # Imports turtle
 tboy = trtl.Turtle() # Makes turtle
-tboy.hideturtle()
+#tboy.hideturtle()
 
-window_color = 255,255,255
-window_border = 207,225,244
-edge_color = 102,112,124
+window_color = "#ffffff"
+window_border = "#cfe1f4"
+edge_color = "#66707c"
+outer_edge = "#4a525b"
 
-tboy.pencolor("#4a525b")
-tboy.forward(274)
+tboy.pencolor(outer_edge)
+tboy.fillcolor(window_border)
+count = 0
+tboy.penup()
+tboy.goto(-137, 68.5)
+tboy.pendown()
+tboy.begin_fill()
+while count < 4:
+    if count%2 == 0:
+        tboy.forward(274)
+    else:
+        tboy.forward(137)
+    tboy.circle(-5, 90)
+    count += 1
+tboy.end_fill()
+tboy.penup()
+tboy.goto(-130, 41.5)
+tboy.pendown()
+small_count = 0
+while small_count < 4:
+    if small_count%2 == 0:
+        tboy.forward(274)
+    else:
+        tboy.forward(137)
+    tboy.right(90)
+    small_count += 1
 
 wn = trtl.Screen()
 wn.mainloop()
